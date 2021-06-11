09e716b64410fc52eb4ee53a2e70f777c21b9514
Fasica Robi <fasicasr@gmail.com>
2021-06-10 23:59:25 -0400

    fixed deleted button

--- controllers/api/projectRoutes.js
+++ controllers/api/projectRoutes.js
@@ -85,4 +85,25 @@ router.put('/:id', withAuth, async (req, res) => {
   }
 });
 
+router.delete('/:id', withAuth, async (req, res) => {
+  try {
+    console.log(req.params.id)
+    const projectData = await Project.destroy({
+      where: {
+        song_id: req.params.id,
+        user_id: req.session.user_id,
+      },
+    });
+
+    if (!projectData) {
+      res.status(404).json({ message: 'No project found with this id!' });
+      return;
+    }
+
+    res.status(200).json(projectData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 module.exports = router;
