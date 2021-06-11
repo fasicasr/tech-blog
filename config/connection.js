661829dcff13607c30bb4f7fa10bef863659c6d1
Fasica Robi <fasicasr@gmail.com>
2021-06-10 21:37:52 -0400

    updated styling of webpage

--- config/connection.js
+++ config/connection.js
@@ -7,14 +7,15 @@ if (process.env.JAWSDB_URL) {
   sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
   sequelize = new Sequelize(
-    "heroku_6c6ab6091e8c633",
-    "b7423e856ee2eb",
-    "d85eb008",
-    // process.env.DB_NAME,
-    // process.env.DB_USER,
-    // process.env.DB_PASSWORD,
+    // "heroku_6c6ab6091e8c633",
+    // "b7423e856ee2eb",
+    // "d85eb008",
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
     {
-      host: 'us-cdbr-east-04.cleardb.com',
+      // host: 'us-cdbr-east-04.cleardb.com',
+      host: 'localhost',
       dialect: 'mysql',
       port: 3306
     }
